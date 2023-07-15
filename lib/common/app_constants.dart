070cc2ba5a9e3f67378c2133acd76465eef6b586
phamtanphat <phatdroid94@gmail.com>
2023-07-15 18:40:57 +0700

    api request

--- lib/common/app_constants.dart
+++ lib/common/app_constants.dart
@@ -4,5 +4,9 @@ class AppConstants {
   // Route
   static const String SIGN_IN_ROUTE_NAME = "/sign-in";
 
+  // Assets
   static const String IMAGE_BANNER_ASSETS = "assets/images/ic_food_express.png";
+
+  // Url
+  static const String SIGN_IN_URL = "user/sign-in";
 }
