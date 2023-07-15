fa29cef6e99b83b9b525fddbc45b16eaab7e0269
phamtanphat <phatdroid94@gmail.com>
2023-07-15 18:52:06 +0700

    app response

---
+++ lib/data/repository/authentication_repository.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_app_sale_25042023/data/api/api_request.dart';
+
+class AuthenticationRepository {
+  ApiRequest? _apiRequest;
+
+  void setApiRequest(ApiRequest apiRequest) {
+    _apiRequest = _apiRequest;
+  }
+
+  Future signInService(String email, String password) {
+    
+  }
+}
