6c2d04d0cb13ce4026f21f76e72144af37ea72ec
phamtanphat <phatdroid94@gmail.com>
2023-07-20 19:35:04 +0700

    Handle sign up

--- lib/common/app_constants.dart
+++ lib/common/app_constants.dart
@@ -3,10 +3,12 @@ class AppConstants {
 
   // Route
   static const String SIGN_IN_ROUTE_NAME = "/sign-in";
+  static const String SIGN_UP_ROUTE_NAME = "/sign-up";
 
   // Assets
   static const String IMAGE_BANNER_ASSETS = "assets/images/ic_food_express.png";
 
   // Url
   static const String SIGN_IN_URL = "user/sign-in";
+  static const String SIGN_UP_URL = "user/sign-up";
 }
